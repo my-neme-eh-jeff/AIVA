9429e6a0836ce705b03f7ed50d8ad508fcbd3322
Innomer <innomershyper@gmail.com>
2024-03-30 21:50:25 +0530

    Voice Auth

--- ML/voice.py
+++ ML/voice.py
@@ -122,4 +122,4 @@ def recognize_speaker_api():
 
 if __name__ == "__main__":
     speaker_models = {}
-    app.run(host='13.200.249.129',debug=True, port=8090)
+    app.run(host='0.0.0.0',debug=True, port=8090)
