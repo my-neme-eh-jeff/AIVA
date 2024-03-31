a07c61967590963e26e1499429cddd943167149e
jeff <aman2003nambisan@gmail.com>
2024-03-31 05:47:48 +0530

    bug: deployement fix

---
+++ src/app/[locale]/(home)/_components/feature-globe-section/projects/descriptions/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./style.module.scss";
+
+export default function Index({
+  data,
+  selectedProject,
+}: {
+  data: { title: string; description: string }[];
+  selectedProject: number;
+}) {
+  const crop = (string: string, maxLength: number) => {
+    return string.substring(0, maxLength);
+  };
+  return (
+    <div className={styles.descriptions}>
+      {data.map((project, i) => {
+        const { title, description } = project;
+        return (
+          <div
+            key={i}
+            className={styles.description}
+            style={{
+              clipPath:
+                selectedProject == i ? "inset(0 0 0)" : "inset(50% 0 50%",
+            }}
+          >
+            <p>{crop(title, 9)}</p>
+            <p>{description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
